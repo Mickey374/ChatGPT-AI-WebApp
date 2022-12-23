cbc3585a1a89f9f199cac816ae51674c17628ef9
Mickey374 <asantemichael37483@gmail.com>
2022-12-23 23:00:04 +0100

    Function for chatStripe on UI completed.

--- client/script.js
+++ client/script.js
@@ -1,46 +1,64 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
 
 // Function to load the messages with the 3 dots
 function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+  }, 300);
+
+  if (element.textContent === "....") {
     element.textContent = "";
-
-    loadInterval = setInterval(() => {
-        element.textContent += '.';
-    }, 300);
-
-    if(element.textContent === '....'){
-        element.textContent = "";
-    }
+  }
 }
 
 //Function to implement the AI response letter by letter
-function typeText(element, text){
-    let index = 0;
-    let interval = setInterval(()=> {
-        //Check if the index is less than text length
-        if(index < text.length){
-            element.innerHTML += text.charAt(index);
-        }
-        //else if we reach end of text, clear the interval
-        else{
-            clearInterval(interval);
-        }
-    }, 20);
-
-
+function typeText(element, text) {
+  let index = 0;
+  let interval = setInterval(() => {
+    //Check if the index is less than text length
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+    }
+    //else if we reach end of text, clear the interval
+    else {
+      clearInterval(interval);
+    }
+  }, 20);
 }
 
 //Function to generate unique ID for each chat to Map over them
-function generateUnique(){
-    const timestamp = Date.now();
-    const randomNumber = Math.random();
-    const hexadecimalString = randomNumber.toString(16);
+function generateUnique() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
 
-    return `id-${timestamp}-${hexadecimalString}`;
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+//Function that segments the color background for AI or User with their images
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div className="profile">
+                    <img 
+                        src="${isAi ? bot : user}"
+                        alt="${isAi ? 'bot' : 'user'}"
+                    />
+                </div>
+                <div class="message" id="${uniqueId}">${value}</div>
+            </div>
+        </div>
+    
+    `
+  )
 }
