b12c6812d21ad5fa414fb778a664249228966cd8
Mickey374 <asantemichael37483@gmail.com>
2022-12-23 22:50:57 +0100

    function to generate a unique id for each chat.

--- client/script.js
+++ client/script.js
@@ -35,3 +35,12 @@ function typeText(element, text){
 
 
 }
+
+//Function to generate unique ID for each chat to Map over them
+function generateUnique(){
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
